0a501fc95f783b04bbad9280f2e80036bf033bdd
Айасын Канапиянов <yaprogist@github.com>
2018-05-26 18:48:13 +0700

    basix

--- calc.c
+++ calc.c
@@ -6,30 +6,96 @@
 #define STACK_OVERFLOW -1
 #define STACK_UNDERFLOW -2
 
-typedef struct Stack {
-	int data[STACK_MAX_SIZE];
+typedef struct Stack { // стек чисел
+	double data[STACK_MAX_SIZE];
 	size_t size;
 } stack_t;
 
-void PushStack(stack_t *stack, const int value);
-int pop(stack_t *stack);
-double number(char *str, size_t *idx);
+typedef struct OperStack { // стек операций
+	char data[STACK_MAX_SIZE];
+	size_t size;
+} operstack;
+
+void PushStack(stack_t *stack, const double value); //заполнение стека числами
+void push_oper_stack(operstack *signstack, const char value); //заполнение стека операций
+double PopStack(stack_t *stack); // удаление вершины стека
+double peek(const stack_t *stack);
+char PopSign(operstack *signstack); // возвращает текущий элемент с вершины стека
+void PrintStackValue(const double value);
+void PrintStack(const stack_t *stack, void (*printStackValue)(const double));
+double Number(char *str, size_t *idx);
+void Eval(char *str);
+char expr(char *str, size_t *idx);
+char multiple(char *str, size_t *idx);
+double calc_multiple(stack_t *stack, operstack *signstack);
+double calc_sum(stack_t *stack, operstack *signstack);
+char PeekSign(const operstack *signstack);
+
 
 int main() {
-	double result;
+	
 	char str[SIZE];
-	size_t i;
 	printf("Enter expression: ");
 	fgets(str, SIZE, stdin);
-	i = 0;
-	result = number(str, &i);
-	printf("\n\n%f\n", result);
+	Eval(str);
+}
 
+void Eval(char *str) {
+	char sign, n;
+	stack_t stack;
+	stack.size = 0;
+	operstack signstack;
+	signstack.size = 0;
+	double result = 0.0;
+	size_t i = 0;
+
+	result = Number(str, &i);
+	printf("\n\n%f\n", result);
+	PushStack(&stack, result);
+	printf("\n");
+	PrintStack(&stack, PrintStackValue);
+
+	sign = expr(str, &i);
+	//if (PeekSign(&signstack)==0) {
+		push_oper_stack(&signstack, sign);
+	//}
+
+	result = Number(str, &i);
+	printf("\n\n%f\n", result);
+	PushStack(&stack, result);
+	printf("\n");
+	PrintStack(&stack, PrintStackValue);
+
+	
+	n = PeekSign(&signstack);
+	sign = expr(str, &i);
+	//if (n==0) {
+		push_oper_stack(&signstack,sign);
+	//}
+	if (n == '*' || n == '/') 
+	{
+		result = calc_multiple(&stack, &signstack); //add проверка peek 
+		PushStack(&stack, result);
+		if (result == -1) {
+			printf("Division by zero");
+			exit(-1);
+		}
+	}
+		push_oper_stack(&signstack, sign);
+	if ((n == '+') || 
+		(n == '-'))
+	{
+		result = calc_sum(&stack, &signstack);
+		PushStack(&stack, result);
+		push_oper_stack(&signstack, sign);
+	}
+	printf("\n\nresultat: %lf\n", peek(&stack));
 
 }
 
-double number(char *str, size_t *idx) {
+double Number(char *str, size_t *idx) {
 	double result = 0.0;
+	double div = 10.0;
 	int sign = 1;
  
 	if (str[*idx] == '-')
@@ -44,12 +110,111 @@ double number(char *str, size_t *idx) {
 		
 		++*idx;
 	}
+
+	if (str[*idx] == '.')
+	{
+		++*idx;
  
+		while (str[*idx] >= '0' && str[*idx] <= '9')
+		{
+			result = result + (str[*idx] - '0') / div;
+			div *= 10.0;
+ 
+			++*idx;
+		}
+	}
+
 	return sign * result;
 
 }
 
-void PushStack(stack_t *stack, const int value) {
+char expr(char *str, size_t *idx) {
+	char sign;
+	//char sign = multiple(str, idx);
+	while (str[*idx] == '+' || str[*idx] == '-')
+    {
+	switch (str[*idx])
+	{
+	case '+':
+		++*idx;
+		sign = '+';
+		break;
+	case '-':
+		++*idx;
+		sign = '-';
+		break;
+	}
+	}
+	return sign;
+}
+
+
+
+char multiple(char *str, size_t *idx) {
+
+	char sign;
+	while (str[*idx] == '*' || str[*idx] == '/')
+    {
+	switch (str[*idx])
+	{
+	case '*':	
+		++*idx;
+		sign = '*';
+		break;
+	case '/':
+		++*idx;
+		sign = '/';
+		break;
+	}
+	}
+	return sign;
+	
+
+}
+
+double calc_multiple(stack_t *stack, operstack *signstack) {
+	char sign;
+	double result = 0.0;
+	sign = PopSign(signstack);
+	switch (sign)
+		{
+			case '*':
+			result = PopStack(stack);
+			result *= PopStack(stack);
+			break;
+			case '/':
+			result = PopStack(stack);
+			if (peek(stack)!=0) {
+				result /= PopStack(stack);
+			}
+			else {
+				return -1;
+			}
+			break;
+		}
+	return result;
+}
+
+double calc_sum(stack_t *stack, operstack *signstack) {
+	char sign;
+	double result = 0.0;
+	sign = PopSign(signstack);
+	switch (sign)
+	{
+		case '+':
+		result = PopStack(stack);
+		result += PopStack(stack);
+		break;
+		case '-':
+		result = PopStack(stack);
+		result -= PopStack(stack);
+		break;
+	}
+	return result;
+}
+
+
+void PushStack(stack_t *stack, const double value) {
 	if (stack->size >= STACK_MAX_SIZE) {
 		exit(STACK_OVERFLOW);
 	}
@@ -57,11 +222,60 @@ void PushStack(stack_t *stack, const int value) {
 	stack->size++;
 }
 
+void push_oper_stack(operstack *signstack, const char value) {
+	if (signstack->size >= STACK_MAX_SIZE) {
+		exit(STACK_OVERFLOW);
+	}
+	signstack->data[signstack->size] = value;
+	signstack->size++;
+}
 
-int pop(stack_t *stack) {
+
+double PopStack(stack_t *stack) {
 	if (stack->size == 0) {
 		exit(STACK_UNDERFLOW);
 	}
 	stack->size--;
 	return stack->data[stack->size];
+}
+
+
+void PrintStackValue(const double value) {
+	printf("%f", value);
+}
+ 
+void PrintStack(const stack_t *stack, void (*printStackValue)(const double)) {
+	int i;
+	int len = stack->size - 1;
+	printf("stack %zd > ", stack->size);
+	for (i = 0; i < len; i++) {
+		printStackValue(stack->data[i]);
+		printf(" | ");
+	}
+	if (stack->size != 0) {
+		printStackValue(stack->data[i]);
+	}
+	printf("\n");
+}
+
+char PopSign(operstack *signstack) { //rename
+    if (signstack->size <= 0) {
+        exit(STACK_UNDERFLOW);
+    }
+    signstack->size--;
+    return signstack->data[signstack->size];
+}
+
+double peek(const stack_t *stack) {
+    if (stack->size <= 0) {
+        exit(STACK_UNDERFLOW);
+    }
+    return stack->data[stack->size - 1];
+}
+
+char PeekSign(const operstack *signstack) {
+    if (signstack->size <= 0) {
+        exit(STACK_UNDERFLOW);
+    }
+    return signstack->data[signstack->size - 1];
 }
---
+++ makefile
@@ -0,0 +1,4 @@
+CC = gcc
+CFLAGS = -Wall
+all:
+	$(CC) $(CFLAGS) -o exe calc.c
