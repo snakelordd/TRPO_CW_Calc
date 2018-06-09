3b92f1f570a664396b1fe5d3b2ccb12b659700fd
Айасын Канапиянов <yaprogist@github.com>
2018-06-09 17:46:18 +0700

    Breakdown complete

---
+++ src/calc.h
@@ -0,0 +1,22 @@
+#ifndef  CALC_H
+#define  CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define SIZE 1024
+
+double number(char *str, size_t *idx);
+
+char summ(char *str, size_t *idx);
+
+char multiple(char *str, size_t *idx);
+
+double factor(char *str, size_t *idx);
+
+double brackets(char *str, size_t *idx);
+
+void clrscr(void);
+
+
+#endif
